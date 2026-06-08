e61005e93da879c8328cd364c74442955375e287
林浩文 <leon050708@outlook.com>
2026-06-08 15:37:08 +0800

    框架搭建

---
+++ src/main/java/com/neusoft/neu23/neuhospital/NeuhospitalApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.neu23.neuhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuhospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuhospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neusoft/neu23/neuhospital/NeuhospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.neu23.neuhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuhospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
